4db68a8347cad991d2bdc4e23f944cc5e55f0935
Dulum <itamar.salhov@gmail.com>
2020-08-25 22:32:20 +0300

    added misc for miscellaneous classes and magic vars

---
+++ misc.py
@@ -0,0 +1,29 @@
+class Resources:
+    def __init__(self, wood, clay, iron, crop):
+        self.vector = np.array([wood, clay, iron, crop])
+    
+    def __add__(self, other):
+        
+        if type(other) == Resources:
+            new_resources = self.vector + other.vector
+        
+        else:
+            new_resources = self.vector + np.array(other)
+            
+        return Resources(*new_resources)
+    
+    def __sub__(self, other):
+        
+        if type(other) == Resources:
+            new_resources = self.vector - other.vector
+        
+        else:
+            new_resources = self.vector - np.array(other)
+            
+        return Resources(*new_resources)
+        
+    def __str__(self):
+        return '|{0}|{1}|{2}|{3}|'.format(*self.vector)
+    
+    def __repr__(self):
+        return self.__str__()
